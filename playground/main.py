df04fb78afbbbaff466adb6f7ab4d1bf13d6329f
Branch Vincent <branchevincent@gmail.com>
2021-02-15 15:56:33 -0500

    test docs

--- playground/main.py
+++ playground/main.py
@@ -1,2 +1,12 @@
-def main():
-    print("Hello, world!")
+import click
+
+
+@click.group()
+def cli():
+    """Main entrypoint."""
+
+
+@cli.command()
+@click.option("-d", "--debug", help="Include debug output.")
+def build(debug):
+    """Build production assets."""
